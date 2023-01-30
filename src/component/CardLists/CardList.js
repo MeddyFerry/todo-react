dc27f0eab53da4f807e563fd62b94ca69dc7e156
Meddy ferry <meddyferry@Mac-Meddy.local>
2023-01-30 14:07:57 +0100

    add h1 + style on CardList component

--- src/component/CardLists/CardList.js
+++ src/component/CardLists/CardList.js
@@ -4,6 +4,9 @@ import TaskCard from "../Tascard/Tascard";
 const CardLists = ({ tasks, onDeleteTask }) => {
   return (
     <div className="w-full p-6 m-auto bg-slate-800 rounded-md shadow-xl shadow-gray-50/40 ring-2 ring-indigo-400 lg:max-w-xl">
+      <h1 className=" text-center tpt-6 bg-clip-text text-transparent bg-gradient-to-b from-gray-100 to-purple-400 font-bold text-lg ">
+        LISTE
+      </h1>{" "}
       {tasks.map((task, index) => (
         <TaskCard
           key={index}
